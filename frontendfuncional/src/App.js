1fd2ca18e32ca1ba63bf9d1d7b51880897ad136f
Herreramartos <herreramartos@gmail.com>
2020-03-28 17:45:40 -0300

    Front-end do app

--- frontendfuncional/src/App.js
+++ frontendfuncional/src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './global.css';
 
-import Routes from './routes';
+import Logon from './pages/Logon';
 
 function App() { 
   return (
-    <Routes />
+    <Logon />
   );
 }
 
