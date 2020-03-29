244bff144c9fb4cba97d2beeb9bf19737c9f33c9
Herreramartos <herreramartos@gmail.com>
2020-03-29 02:53:35 -0300

    Mobile da Aplicação

---
+++ mobile/src/services/api.js
@@ -0,0 +1,10 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL:'http://192.168.0.103:3333'
+})
+
+export default api;
